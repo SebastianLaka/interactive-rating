cc35ec1b8237bc0dd8f87049f373f8aa40db0dc1
Sebastian <sebastianlakla@gmail.com>
2022-11-05 19:00:21 +0100

    create function and create addEventListener

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
-const buttonsFromDiv = document.querySelectorAll('.button-section__btn');
-const primaryButton = document.querySelector('.first-display-card__primary-button');
-console.log(buttonsFromDiv);
-console.log(primaryButton);
+const buttonsFromDiv = document.querySelectorAll(".button-section__btn");
+const primaryButton = document.querySelector(
+  ".first-display-card__primary-button"
+)
+const buttonsClick = () => {
+  if () {
+    
+  }
+}
+
+primaryButton.addEventListener('click', buttonsClick)
