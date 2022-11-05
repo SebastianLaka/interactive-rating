e500ae8704bfc0512169171c509f2b2b32eaf30a
Sebastian <sebastianlakla@gmail.com>
2022-11-05 18:02:32 +0100

    Check buttons

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const buttonsFromDiv = document.querySelectorAll('.button-section__btn');
+const primaryButton = document.querySelector('.first-display-card__primary-button');
+console.log(buttonsFromDiv);
+console.log(primaryButton);
