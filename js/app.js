3156f4e7b0f4356aed4bd66ca5a793bc5ce54353
Sebastian <sebastianlakla@gmail.com>
2022-11-07 20:46:15 +0100

    create function to interaction with submit button

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
-const firstDisplayCard = document.querySelector(".first-display-card")
-const secondDisplayCard = document.querySelector(".second-display-card")
-const buttons = document.querySelectorAll(".button-section__btn")
-const submitButton = document.getElementById("submit")
-const choosenButton = document.getElementById("selected-button")
+const firstCard = document.querySelector(".first-display-card");
+const secondCard = document.querySelector(".second-display-card");
+const buttons = document.querySelectorAll(".button-section__btn");
+const submitButton = document.getElementById("submit");
+const choosenButton = document.getElementById("selected-button");
 
+submitButton.addEventListener("click", () => {
+  secondCard.classList.remove("hidden");
+  firstCard.style.display = "none";
+});
