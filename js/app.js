f7a88f3d7189203c880b78ab71e85f0b08fdd46b
Sebastian <sebastianlakla@gmail.com>
2022-11-11 12:48:53 +0100

    End project

--- js/app.js
+++ js/app.js
@@ -2,11 +2,13 @@ const firstCard = document.querySelector(".first-display-card");
 const secondCard = document.querySelector(".second-display-card");
 const buttons = document.querySelectorAll(".button-section__btn");
 const submitButton = document.getElementById("submit");
-const choosenButton = document.getElementById("selected-button");
+const chosenButton = document.getElementById("chosen-number");
 
 submitButton.addEventListener("click", () => {
   secondCard.classList.remove("hidden");
   firstCard.style.display = "none";
 });
 
+buttons.forEach(button => button.addEventListener('click', ()=>
+chosenButton.innerHTML = button.innerHTML));
 
